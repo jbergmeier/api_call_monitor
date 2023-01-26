1cf966987768207b4cde945f6df102c5eadcccbf
Bergmeier <joern.bergmeier@arvato-scs.com>
2023-01-26 15:52:26 +0100

    basic setup running

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+// Create Express Instance
+const app = express();
+const port = process.env.PORT || 1337;
+
+// Middleware
+app.use(cors());
+app.use(morgan('tiny'));
+
+// Endpoint definition
+app.post('/', (req, res) => {
+    console.log("Hello")
+    res.json({'message': 'hello, it is working'})
+})
+
+// start server
+app.listen(port, () => {
+    console.log(`Server listening on Port: ${port}`);
+  });
