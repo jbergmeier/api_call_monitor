a9f77f918de5b1b1728afa7bf49c14514c6cf9fa
Joern Bergmeier <jbergmeier@jbergmeier.com>
2023-01-31 16:58:49 +0100

    cleaning

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
-var Hjson = require('hjson');
+
 require('dotenv').config();
 const fs = require('fs/promises');
 
@@ -12,9 +12,6 @@ const port = process.env.PORT || 1337;
 // Middleware
 app.use(cors());
 app.use(morgan('tiny'));
-
-// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse various different custom JSON types as JSON
 app.use(express.json());
@@ -43,20 +40,6 @@ app.post('/', (req, res) => {
     // Send response
     //res.json(JSON.stringify(req.body))
   res.json(req.body)
-})
-
-app.get('/', (req, res) => {
-    // assign apiKey to the header value x-api-key
-    let apiKey = req.header('x-api-key')
-
-    // Check if apiKey is filled, else set value to unix timestamp of the current time
-    apiKey ? apiKey : apiKey = Date.now().toString()
-    
-    // call file creation function
-    example(apiKey, "GET-Request No Body")
-    
-    // Send response
-    res.send("Successful "+ apiKey)
 })
 
 // start server
